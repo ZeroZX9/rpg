44183479f925ec2c06db27ab26865588e480c253
Leandro Dias <ldias@processwaregroup.com>
2022-09-21 20:38:51 -0300

    Update Program.cs

--- game01/game01/Program.cs
+++ game01/game01/Program.cs
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Alteração Leandro!");
-=======
-﻿//parte principal do programa
 
 var hp = 0;
->>>>>>> f6c9ecfd3dd9e7ff9a7cfddbfb611f114b190e7f
+
