c893603826d753c0ce38225940f175338efa9d0f
Pablo Almeida <pablonike10.pj@gmail.com>
2022-09-25 00:32:30 -0300

    update v0.0.1 "creature"
    add creature class

--- game01/game01/Program.cs
+++ game01/game01/Program.cs
@@ -1,3 +1,18 @@
+using System.Runtime.InteropServices;
 
-var hp = 5;
+static void Main(string[] args) 
+{
+    creature goblin = new creature();
+}
+public class creature {
+    string name { get; set; }
+    int hp { get; set; }
+    int def { get; set; }
+    int mp { get; set; }
+    int atk { get; set; }
+    int spd { get; set; }
+    int xp { get; set; }
+    int lvl { get; set; }
+
+}
 
